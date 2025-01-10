e2a505ebbbac12f1f1a5357253b3f46c295bdc72
penso996 <penso.andrea@outlook.it>
2025-01-10 14:14:26 +0100

    ex 4 done

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,19 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function vowelCounter(string) {
+    let vowelNumber = 0;
+    for (let i = 0; i < string.length; i++) {
+        let letter = string[i];
+        if ("aeiou".includes(letter)) {
+            vowelNumber += 1;
+        }
+    }
+    return vowelNumber;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(vowelCounter(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
